d45913bf4184f80ee3bf906af9efcefe70b01196
jiangtaows <taoJiang_ict@163.com>
2016-04-16 19:14:35 +0800

    安装开发环境：        python2.7+scrapy1.0.5 测试安装成功：        命令行中输入scrapy crawl dmoz

---
+++ main.py
@@ -0,0 +1,13 @@
+import scrapy
+
+class BlogSpider(scrapy.Spider):
+    name = 'blogspider'
+    start_urls = ['https://blog.scrapinghub.com']
+
+    def parse(self, response):
+        for url in response.css('ul li a::attr("href")').re('.*/category/.*'):
+            yield scrapy.Request(response.urljoin(url), self.parse_titles)
+
+    def parse_titles(self, response):
+        for post_title in response.css('div.entries > ul > li a::text').extract():
+            yield {'title': post_title}
