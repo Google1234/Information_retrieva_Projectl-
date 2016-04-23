c280d170b7c599c13ec8779d1b80d3c5099f5009
jiangtaows <taoJiang_ict@163.com>
2016-04-23 23:03:10 +0800

    yield 和回调函数的问题没有解决

--- tutorial/tutorial/items.py
+++ tutorial/tutorial/items.py
@@ -6,17 +6,13 @@
 # http://doc.scrapy.org/en/latest/topics/items.html
 
 import scrapy
-class DmozSpider(scrapy.Spider):
-    name = "dmoz"
-    allowed_domains = ["dmoz.org"]
-    start_urls = [
-        "http://www.dmoz.org/Computers/Programming/Languages/Python/Books/",
-        "http://www.dmoz.org/Computers/Programming/Languages/Python/Resources/"
-    ]
 
-    def parse(self, response):
-        for sel in response.xpath('//ul/li'):
-            title = sel.xpath('a/text()').extract()
-            link = sel.xpath('a/@href').extract()
-            desc = sel.xpath('text()').extract()
-            print title, link, desc
+
+class neteaseItem(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    keywords=scrapy.Field()#关键字
+    title=scrapy.Field()   #标题
+    content=scrapy.Field() #正文
+    link=scrapy.Field()    #链接
+    pass
